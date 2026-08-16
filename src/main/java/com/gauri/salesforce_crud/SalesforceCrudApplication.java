9892d43fd29125f97eb60735d81c93da846ab211
Gauri thokal <gaurithokal2004@gmail.com>
2026-08-17 01:29:08 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/gauri/salesforce_crud/SalesforceCrudApplication.java
@@ -0,0 +1,13 @@
+package com.gauri.salesforce_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesforceCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesforceCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gauri/salesforce_crud/SalesforceCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gauri.salesforce_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesforceCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
